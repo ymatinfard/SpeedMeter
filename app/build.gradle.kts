169ed7bca28e7b9f5e6a45340875ff4d1e8c04be
Yousef <matinfard.y@gmail.com>
2025-02-06 19:04:53 +0100

    Add hilt plugin to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.speedmeter.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.speedmeter.android.hilt)
 }
 
 android {
@@ -40,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation(projects.core.network)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -48,6 +50,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
