d7a70d7cc435bc57b59d05126b3de26b3f51ce8f
Yousef <matinfard.y@gmail.com>
2025-02-06 17:42:56 +0100

    Add android application convention plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.speedmeter.android.application)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.matin.speedmeter"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.matin.speedmeter"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
