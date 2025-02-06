27569740166b500c8b76f1437396e1e79aea15b6
Yousef <matinfard.y@gmail.com>
2025-02-06 22:53:16 +0100

    Chore AndroidLibraryConventionPlugin moved

--- build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -1,6 +1,5 @@
-package com.matin.speedmeter
-
 import com.android.build.gradle.LibraryExtension
+import com.matin.speedmeter.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
