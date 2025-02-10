39830dd1b70cf578363f09b17c8553012059d1cf
Yousef <matinfard.y@gmail.com>
2025-02-10 17:35:13 +0100

    Add worker delegation to ease setting configuration

--- sync/src/main/java/com/matin/sync/Sync.kt
+++ sync/src/main/java/com/matin/sync/Sync.kt
@@ -1,7 +1,7 @@
 package com.matin.sync
 
 import android.content.Context
-import androidx.work.ExistingWorkPolicy
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.WorkManager
 
 internal const val SYNC_WORK_NAME = "sync_work_name"
@@ -9,7 +9,7 @@ internal const val SYNC_WORK_NAME = "sync_work_name"
 object Sync {
     fun init(context: Context) {
         WorkManager.getInstance(context).apply {
-            enqueueUniqueWork(SYNC_WORK_NAME, ExistingWorkPolicy.KEEP, SyncWorker.startUpSyncWork())
+            enqueueUniquePeriodicWork(SYNC_WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, SyncWorker.periodicSyncWork())
         }
     }
 }
