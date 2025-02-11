56fe9dfd6bdd87b0e88aaa6368299a8ab2dc420d
Yousef <matinfard.y@gmail.com>
2025-02-11 21:29:11 +0100

    Refactor distance setup screen

--- feature/stopwatch/src/main/java/com/matin/feature/stopwatch/DistanceSetupScreen.kt
+++ feature/stopwatch/src/main/java/com/matin/feature/stopwatch/DistanceSetupScreen.kt
@@ -1,28 +1,14 @@
 package com.matin.feature.stopwatch
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,14 +17,21 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.matin.core.designsystem.theme.SpeedMeterTheme
 import com.matin.core.designsystem.theme.component.SpeedMeterTopBar
 import com.matin.feature.stopwatch.model.CurrentSelectedPlayer
+import com.matin.speedmeter.feature.stopwatch.R
 
 @Composable
-fun DistanceSetupScreen(viewModel: StopWatchSharedViewModel, onBack: () -> Unit, onNavigateToStopWatch: () -> Unit) {
-    val selectedPlayer = viewModel.currentSelectedPlayer.collectAsStateWithLifecycle()
+fun DistanceSetupScreen(
+    viewModel: StopWatchSharedViewModel,
+    onBack: () -> Unit,
+    onNavigateToStopWatch: () -> Unit
+) {
+    val selectedPlayer by viewModel.currentSelectedPlayer.collectAsStateWithLifecycle()
 
-    DistanceSetupScreenContent(selectedPlayer.value, onBack,) { distance ->
+    DistanceSetupScreenContent(
+        selectedPlayer = selectedPlayer,
+        onBack = onBack
+    ) { distance ->
         viewModel.setCurrentSelectedPlayer(distance = distance)
-
         onNavigateToStopWatch()
     }
 }
@@ -50,47 +43,60 @@ private fun DistanceSetupScreenContent(
     onBack: () -> Unit,
     onNavigateToStopWatch: (distance: Float) -> Unit
 ) {
+    var distance by remember { mutableStateOf("") }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         containerColor = MaterialTheme.colorScheme.background,
         topBar = {
-            SpeedMeterTopBar("Distance Setup", onBack = onBack)
+            SpeedMeterTopBar(title = stringResource(id = R.string.feature_stopwatch_distance_setup), onBack = onBack)
         }
-    ) {
-        var distance by remember { mutableStateOf("10") }
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
             Column(
-                modifier = Modifier
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                CircularImage(imageUrl = selectedPlayer.player?.imageUrl.toString())
+                CircularImage(imageUrl = selectedPlayer.player?.imageUrl.orEmpty())
+
                 Spacer(Modifier.height(30.dp))
+
                 Text(
-                    text = selectedPlayer.player?.fullName.toString(),
+                    text = selectedPlayer.player?.fullName.orEmpty(),
                     style = MaterialTheme.typography.titleLarge,
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
+
                 OutlinedTextField(
                     value = distance,
-                    onValueChange = { distance = it },
-                    label = { Text("Distance in Meter") },
+                    onValueChange = {
+                        if (it.all { char -> char.isDigit() || char == '.' }) {
+                            distance = it.trim()
+                        }
+                    },
+                    label = { Text(stringResource(id = R.string.feature_stopwatch_distance_in_meter)) },
                     modifier = Modifier.width(200.dp),
                     maxLines = 1,
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Number
-                    ),
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
                     textStyle = MaterialTheme.typography.bodyLarge
                 )
+
                 Spacer(modifier = Modifier.height(26.dp))
-                val distanceValue = distance.toFloatOrNull() ?: 0f
+
+                val distanceValue = distance.toFloatOrNull()
+
                 Button(
-                    onClick = { onNavigateToStopWatch(distance.toFloat()) },
-                    enabled = distanceValue > 0f,
+                    onClick = { distanceValue?.let { onNavigateToStopWatch(it) } },
+                    enabled = distance.isNotBlank() && distanceValue != null && distanceValue > 0f,
                     shape = RoundedCornerShape(12.dp),
                     colors = ButtonDefaults.buttonColors(disabledContentColor = MaterialTheme.colorScheme.onSecondaryContainer)
                 ) {
-                    Text(text = "Start Training Session", fontSize = 18.sp)
+                    Text(text = stringResource(id = R.string.feature_stopwatch_start_training_session), fontSize = 18.sp)
                 }
             }
         }
