66fc77f943cdf0eeaa7aa7c9ddd3971d2b7ef20a
Yousef <matinfard.y@gmail.com>
2025-02-12 13:59:25 +0100

    Add metric summary in player metric chart screen

---
+++ core/common/src/main/java/com/matin/core/common/Util.kt
@@ -0,0 +1,3 @@
+package com.matin.core.common
+
+fun Long.milliSecondToSecond() = this / 1000f
--- feature/stopwatch/src/main/java/com/matin/feature/stopwatch/PlayerMetricChartScreen.kt
+++ feature/stopwatch/src/main/java/com/matin/feature/stopwatch/PlayerMetricChartScreen.kt
@@ -1,24 +1,36 @@
 import androidx.compose.animation.core.EaseInOutCubic
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.matin.core.common.TimeFormatter
 import com.matin.core.designsystem.theme.component.PlayerInfo
 import com.matin.core.designsystem.theme.component.SpeedMeterTopBar
 import com.matin.feature.stopwatch.StopWatchSharedViewModel
 import com.matin.feature.stopwatch.model.UiLeaderBoardPlayer
+import com.matin.speedmeter.feature.stopwatch.R
 import ir.ehsannarmani.compose_charts.LineChart
 import ir.ehsannarmani.compose_charts.models.AnimationMode
 import ir.ehsannarmani.compose_charts.models.DrawStyle
@@ -35,17 +47,29 @@ fun PlayerMetricChartScreen(id: Int, viewModel: StopWatchSharedViewModel, onBack
 fun PlayerMetricChartContent(player: UiLeaderBoardPlayer, onBack: () -> Unit) {
     val minLapTime = player.laps.minOf { it.lapTime }
     val chartData = player.laps.map { (it.lapTime.toDouble() - minLapTime.toDouble()) / 1000 }
+    val scrollState = rememberScrollState()
 
     Scaffold(
-        topBar = { SpeedMeterTopBar(title = "Player Metric Chart", onBack = onBack) }
+        topBar = {
+            SpeedMeterTopBar(
+                title = stringResource(R.string.feature_stopwatch_player_metric_chart),
+                onBack = onBack
+            )
+        }
     ) { padding ->
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(scrollState)
                 .padding(padding),
         ) {
             PlayerInfo(player.fullName, player.distance, player.imageUrl)
             Spacer(modifier = Modifier.height(22.dp))
+            LapTimeSummary(
+                bestLapTime = player.laps.minOf { it.lapTime },
+                worstLapTime = player.laps.maxOf { it.lapTime },
+                averageLapTime = player.laps.map { it.lapTime }.average().toLong()
+            )
             Chart(chartData)
         }
     }
@@ -54,7 +78,7 @@ fun PlayerMetricChartContent(player: UiLeaderBoardPlayer, onBack: () -> Unit) {
 @Composable
 fun Chart(data: List<Double>) {
     val gradientFillColor = MaterialTheme.colorScheme.tertiary
-
+    val chartLabel = stringResource(R.string.feature_stopwatch_time_difference_between_best_lap_s)
     LineChart(
         modifier = Modifier
             .fillMaxWidth()
@@ -63,7 +87,7 @@ fun Chart(data: List<Double>) {
         data = remember {
             listOf(
                 Line(
-                    label = "Lap time(s)",
+                    label = chartLabel,
                     values = data,
                     color = SolidColor(Color(0xFF23af92)),
                     firstGradientFillColor = gradientFillColor,
@@ -79,3 +103,75 @@ fun Chart(data: List<Double>) {
         }),
     )
 }
+
+@Composable
+fun LapTimeSummary(
+    bestLapTime: Long,
+    worstLapTime: Long,
+    averageLapTime: Long,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.feature_stopwatch_lap_time_summary),
+            style = MaterialTheme.typography.labelMedium,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        TimeCard(
+            title = stringResource(R.string.feature_stopwatch_best_lap),
+            time = bestLapTime,
+            backgroundColor = Color.Green.copy(alpha = .3f)
+        )
+        TimeCard(
+            title = stringResource(R.string.feature_stopwatch_worst_lap),
+            time = worstLapTime,
+            backgroundColor = Color.Red.copy(alpha = .3f)
+        )
+        TimeCard(
+            title = stringResource(R.string.feature_stopwatch_average_lap),
+            time = averageLapTime,
+            backgroundColor = MaterialTheme.colorScheme.surfaceVariant
+        )
+    }
+}
+
+@Composable
+fun TimeCard(
+    title: String,
+    time: Long,
+    backgroundColor: Color
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = cardColors(containerColor = backgroundColor)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = TimeFormatter.formatTime(time),
+                    style = MaterialTheme.typography.labelMedium,
+                )
+            }
+        }
+    }
+}
+
