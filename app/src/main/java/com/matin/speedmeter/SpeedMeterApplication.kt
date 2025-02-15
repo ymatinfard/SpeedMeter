78a775f60a26f4f725a1d31ffa2d3ba15b1a6390
Yousef <matinfard.y@gmail.com>
2025-02-15 15:12:30 +0100

    Add strictmode policy to detect potential problems on MainThread

--- app/src/main/java/com/matin/speedmeter/SpeedMeterApplication.kt
+++ app/src/main/java/com/matin/speedmeter/SpeedMeterApplication.kt
@@ -1,6 +1,8 @@
 package com.matin.speedmeter
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
 import com.matin.sync.Sync
 import dagger.hilt.android.HiltAndroidApp
 
@@ -9,5 +11,24 @@ class SpeedMeterApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Sync.init(this)
+
+        setStrictModePolicy()
+    }
+
+    private fun isDebuggable(): Boolean {
+        return applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
+    }
+
+    /**
+     * Set a thread policy that detects all potential problems on the main thread, such as network
+     * and disk access.
+     *
+     * If a problem is found, the offending call will be logged and the application will be killed.
+     */
+    private fun setStrictModePolicy() {
+        if (isDebuggable()) {
+            val builder = StrictMode.VmPolicy.Builder()
+            StrictMode.setVmPolicy(builder.build())
+        }
     }
 }
